1ad546440ac5b5b4170e6598527875e64cdbdc30
joshuabloemer <joshuabloemer@gmail.com>
2023-01-03 15:16:43 +0100

    Simple command line metadata reader

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MetadataExtractor;
+
+IEnumerable<MetadataExtractor.Directory> directories = ImageMetadataReader.ReadMetadata(Path.GetFullPath(args[0]));
+foreach (var directory in directories)
+    foreach (var tag in directory.Tags)
+        Console.WriteLine($"{directory.Name} - {tag.Name} = {tag.Description}");
